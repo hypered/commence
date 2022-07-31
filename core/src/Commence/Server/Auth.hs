e6a45c02fc90b0b8ef1d55e847ead881fd411981
Ashesh Ambasta <ashesh.ambasta@gmail.com>
2022-07-31 11:22:58 +0200

    Add a `PostLogoutHeaders` type.

--- core/src/Commence/Server/Auth.hs
+++ core/src/Commence/Server/Auth.hs
@@ -11,6 +11,7 @@ Description: Authentication and authorization module.
 module Commence.Server.Auth
   ( UserAuthentication
   , PostAuthHeaders
+  , PostLogoutHeaders
   ) where
 
 import           Commence.Types.NonEmptyText   as NE
@@ -23,8 +24,14 @@ type UserAuthentication user = SAuth.Auth '[SAuth.Cookie] user
 
 -- | Headers that will be returned post a successful authentication.
 type PostAuthHeaders
-  = '[ Header "Location" NE.NonEmptyText
-     , Header "Set-Cookie" SAuth.SetCookie
+  = '[ Header "Location" NE.NonEmptyText -- redirect the client to some location.
+     , Header "Set-Cookie" SAuth.SetCookie -- (this and below) set the cookie(s) for authentication.
      , Header "Set-Cookie" SAuth.SetCookie
      ]
 
+-- | Logout headers are the same as login headers (`PostAuthHeaders` above).
+type PostLogoutHeaders
+  = '[ Header "Location" NE.NonEmptyText -- redirect the client to some location after logging out.
+     , Header "Set-Cookie" SAuth.SetCookie -- (this and below) clear the cookie(s).
+     , Header "Set-Cookie" SAuth.SetCookie
+     ]
